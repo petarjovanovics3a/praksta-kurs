2c6349aa4087e89b4d486324af9b23b85d8895c4
petarjovanovics3a <83191905+petarjovanovics3a@users.noreply.github.com>
2021-04-26 16:26:54 +0200

    Update config.php

--- shareposts/app/config/config.php
+++ shareposts/app/config/config.php
@@ -4,6 +4,6 @@
   define('DB_PASS', '123456');
   define('DB_NAME', 'shareposts');
   define('APPROOT', dirname(dirname(__FILE__)));
-  define('URLROOT', 'http://localhost/shareposts');
+  define('URLROOT', 'http://37.0.71.52/s4f.test/jovanovic.petar/shareposts');
   define('SITENAME', 'SharePosts');
   define('APPVERSION', '1.0.0');
