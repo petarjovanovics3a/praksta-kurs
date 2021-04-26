6fb76a372d96b370573c03e9c97483c610fb7092
petarjovanovics3a <83191905+petarjovanovics3a@users.noreply.github.com>
2021-04-26 16:16:56 +0200

    Initial commit

---
+++ shareposts/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  define('DB_HOST', 'localhost');
+  define('DB_USER', 'root');
+  define('DB_PASS', '123456');
+  define('DB_NAME', 'shareposts');
+  define('APPROOT', dirname(dirname(__FILE__)));
+  define('URLROOT', 'http://localhost/shareposts');
+  define('SITENAME', 'SharePosts');
+  define('APPVERSION', '1.0.0');
